c1ee09ad3e7913dfd514a91f8abc32629ba1c150
Patrick Napper <patrick@patricknapper.com>
2018-01-12 13:48:44 -0800

    add ProductController tests for: Confirms model as list of products, Confirms presence of known entry, Confirms Create and Details view functioning.

--- GummiBearKingdom.Tests/ControllerTests/ProductControllerTests.cs
+++ GummiBearKingdom.Tests/ControllerTests/ProductControllerTests.cs
@@ -41,5 +41,85 @@ namespace GummiBearKingdom.Tests.ControllerTests
             // Assert
             CollectionAssert.Contains(collection, testProduct);
         }
+
+        [TestMethod]
+        public void Mock_IndexContainsModelData_List() // Confirms model as list of products
+        {
+            // Arrange
+            DbSetup();
+            ViewResult indexView = new ProductsController(mock.Object).Index() as ViewResult;
+
+            // Act
+            var result = indexView.ViewData.Model;
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(List<Product>));
+        }
+
+        [TestMethod]
+        public void Mock_IndexModelContainsProducts_Collection() // Confirms presence of known entry
+        {
+            // Arrange
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            Product testProduct = new Product();
+            testProduct.Name = "5 Lb. Bag (Assorted Flavors)";
+            testProduct.Price = 12.99m;
+            testProduct.Description = "Yummi Gummis!";
+            testProduct.ProductId = 1;
+
+            // Act
+            ViewResult indexView = controller.Index() as ViewResult;
+            List<Product> collection = indexView.ViewData.Model as List<Product>;
+
+            // Assert
+            CollectionAssert.Contains(collection, testProduct);
+        }
+        [TestMethod]
+        public void Mock_PostViewResultCreate_ViewResult()
+        {
+            // Arrange
+            Product testProduct = new Product
+            {
+                ProductId = 1,
+                Name = "5 Lb. Bag (Assorted Flavors)",
+                Price = 12.99m,
+                Description = "Yummi Gummis!"
+            };
+
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+
+            // Act
+            var resultView = controller.Create(testProduct) as ViewResult;
+
+
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
+
+        }
+        [TestMethod]
+        public void Mock_GetDetails_ReturnsView()
+        {
+            // Arrange
+            Product testProduct = new Product
+            {
+                ProductId = 1,
+                Name = "5 Lb. Bag (Assorted Flavors)",
+                Price = 12.99m,
+                Description = "Yummi Gummis!"
+            };
+
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+
+            // Act
+            var resultView = controller.Details(testProduct.ProductId) as ViewResult;
+            var model = resultView.ViewData.Model as Product;
+
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
+            Assert.IsInstanceOfType(model, typeof(Product));
+        }
     }
 }
